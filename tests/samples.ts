838838f9eaa4a3f9a86b68374ea84767350b0155
mohan.poojary <mohan.poojary@manipal.net>
2025-05-23 22:43:31 +0530

    samples

---
+++ tests/samples.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("Home page", ()=> {
+  test.beforeEach(async({page})=>{
+    await page.goto("https://playwright.dev/docs/intro")
+  })
+  test("Should have correct metadata", async({page})=>{
+    await expect(page).toHaveTitle("Installation | Playwright")
+
+    await expect(page.getByRole("heading", {name: "Installation"})).toBeVisible()
+  })
+// test nav
+  test("check navigation", async({page})=>{
+    await page.getByRole("link", {name: "Running Tests"}).click()
+
+    await expect(page.getByRole("heading", {name: "Running and debugging tests"})).toBeVisible()
+  })
+})
